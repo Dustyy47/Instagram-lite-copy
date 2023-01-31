5c5a3a8bfba7c44d52caccab17c9149a0186c5b3
Dmitry <109687670+Dustyy47@users.noreply.github.com>
2023-01-31 17:03:36 +0300

    *ADAPTIVE* Fixed view of posts

--- client/src/components/Posts/PostsList.jsx
+++ client/src/components/Posts/PostsList.jsx
@@ -6,19 +6,31 @@ import styles from './PostsList.module.scss'
 export function PostsList({ onLike, likedPosts, onClickPost }) {
     const posts = useSelector((state) => state.profile.posts)
 
-    return (
-        <div className={styles.wrapper}>
-            {posts
-                ? posts.map((post) => (
-                      <Post
-                          onClick={onClickPost}
-                          isLiked={isPostLiked(post._id, likedPosts)}
-                          onLike={onLike}
-                          key={post?._id}
-                          data={post}
-                      />
-                  ))
-                : 'Нет постов'}
-        </div>
-    )
+    function generatePost(post) {
+        if (!post) return null
+        return (
+            <Post
+                onClick={onClickPost}
+                isLiked={isPostLiked(post._id, likedPosts)}
+                onLike={onLike}
+                key={post?._id}
+                data={post}
+            />
+        )
+    }
+
+    function generatePostRows(posts) {
+        let rows = []
+        for (let i = 0; i < posts.length; i += 3) {
+            let row = (
+                <div className={styles.row}>
+                    {[0, 1, 2].map((offset) => generatePost(posts[i + offset]))}
+                </div>
+            )
+            rows.push(row)
+        }
+        return rows
+    }
+
+    return <div className={styles.wrapper}>{posts ? generatePostRows(posts) : 'Нет постов'}</div>
 }
